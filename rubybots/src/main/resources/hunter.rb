5924820b2b3b6257e6987ec35ad8ba51d4803c12
crd1 <rudch@web.de>
2017-05-14 21:37:54 +0200

    updated hunter.rb strategy

--- rubybots/src/main/resources/hunter.rb
+++ rubybots/src/main/resources/hunter.rb
@@ -1,18 +1,15 @@
+$context.battlefield.fire($context.battlefield.getMyPosition() + 1) # clear the way
+$context.battlefield.move() # move one step
 1.times do
-	$context.battlefield.move()
+	target=rand($context.battlefield.size)
+	detectedBot=$context.battlefield.whoIsAtPosition(target)
+	if detectedBot == $context.botNumber
+		$context.log("OK, let's commit suicide!")
+	elsif detectedBot == nil
+		$context.log("Just firing for fun.")
+	else
+		$context.log("Aiming at bot #{detectedBot}.")
+	end
+	$context.battlefield.fire(target) # fire anywhere
 end
-$context.battlefield.mine($context.battlefield.getMyPosition())
-target=rand($context.battlefield.size)
-detectedBot=$context.battlefield.whoIsAtPosition(target)
-if detectedBot != nil
-	$context.log("Bot #{detectedBot} at position #{target}.")
-end
-if detectedBot == $context.botNumber
-	$context.log("OK, let's commit suicide!")
-elsif detectedBot == nil
-	$context.log("Just firing for fun.")
-else
-	$context.log("Aiming at bot #{detectedBot}.")
-end
-$context.battlefield.fire(target)
-$context.battlefield.fire(rand($context.battlefield.size))
+$context.battlefield.mine($context.battlefield.getMyPosition()) # mine the position we were at
